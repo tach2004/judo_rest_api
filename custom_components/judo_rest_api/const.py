3acbb321b98c6ff598100ec7caa652dc206b3132
OStrama <66820413+OStrama@users.noreply.github.com>
2024-12-16 11:50:08 +0100

    Create const.py

---
+++ custom_components/judo_rest_api/const.py
@@ -0,0 +1,105 @@
+"""Constants."""
+
+from dataclasses import dataclass
+from datetime import timedelta
+
+from homeassistant.const import (
+    CONF_HOST,
+    CONF_PORT,
+    CONF_PASSWORD,
+    CONF_USERNAME,
+)
+
+
+@dataclass(frozen=True)
+class ConfConstants:
+    """Constants used for configurastion"""
+    HOST = CONF_HOST
+    PORT = CONF_PORT
+    PASSWORD = CONF_PASSWORD
+    USERNAME = CONF_USERNAME
+    DEVICE_POSTFIX = "Device-Postfix"
+    NAME_DEVICE_PREFIX = "Name-Device-Prefix"
+    NAME_TOPIC_PREFIX = "Name-Topic-Prefix"
+
+CONF = ConfConstants()
+
+
+@dataclass(frozen=True)
+class MainConstants:
+    """Main constants."""
+
+    DOMAIN = "judo_rest_api"
+    SCAN_INTERVAL = timedelta(seconds=30)
+    UNIQUE_ID = "unique_id"
+    APPID = 100
+    DEF_PREFIX = "judo_rest_api"
+
+
+CONST = MainConstants()
+
+
+@dataclass(frozen=True)
+class FormatConstants:
+    """Format constants."""
+
+    TEMPERATUR = "temperature"
+    PERCENTAGE = "percentage"
+    NUMBER = "number"
+    STATUS = "status"
+    UNKNOWN = "unknown"
+
+
+FORMATS = FormatConstants()
+
+
+@dataclass(frozen=True)
+class TypeConstants:
+    """Type constants."""
+
+    SENSOR = "Sensor"
+    SENSOR_CALC = "Sensor_Calc"
+    SELECT = "Select"
+    NUMBER = "Number"
+    NUMBER_RO = "Number_RO"
+
+TYPES = TypeConstants()
+
+
+COMMANDS = {
+    "Geraetetyp": "FF00",
+    "Geraetenummer": "0600",
+    "SW-Version": "0100",
+    "Inbetriebnahmedatum": "0E00",
+    "Betriebsstundenzaehler": "2500",
+    "Kundendienst-Serviceadresse": "5800",
+    "Wunschwasserhaerte": "5100",
+    "Salzvorrat": "5600",
+    "Salzreichweite": "5700",
+    "Salzreichweite": "5700",
+    "Gesamtwassermenge": "2800",
+    "Weichwassermenge": "2900",
+    "Tagesstatistik": "FB00",
+    "Wochenstatistik": "FC00",
+    "Monatsstatistik": "FD00",
+    "Jahresstatistik": "FE00",
+}
+
+
+DEVICESTYPES = {
+    "33": "i-soft SAFE+",
+    "42": "i-soft K SAFE+",
+    "58": "i-soft PRO",
+    "4B": "i-soft PRO",
+    "4C": "i-soft PRO L",
+    "32": "i-soft",
+    "43": "i-soft K",
+    "34": "SOFTwell P",
+    "35": "SOFTwell S",
+    "36": "SOFTwell K",
+    "47": "SOFTwell KP",
+    "72": "SOFTwell KS",
+    "44": "ZEWA/PROM i-SAFE (FILT)",
+    "41": "i-dos eco",
+    "3c": "i-fill",
+}
