c17dcd53390efc9f1ddd9f8ee789094bedc65352
OStrama <66820413+OStrama@users.noreply.github.com>
2024-12-18 08:53:31 +0100

    move formatting to restobject

--- custom_components/judo_rest_api/restobject.py
+++ custom_components/judo_rest_api/restobject.py
@@ -83,6 +83,26 @@ class RestAPI:
             log.warning("Connection to Judo Water Treatment failed")
             return None
 
+    async def set_rest(self, command: str, towrite: str):
+        """write raw response to REST api"""
+        try:
+            url = self._api_url + command + towrite
+            response = await self._hass.async_add_executor_job(
+                partial(
+                    requests.get,
+                    url=url,
+                    auth=(self._username, self._password),
+                    timeout=10,
+                )
+            )
+            res = await self._hass.async_add_executor_job(response.json)
+            return res["data"]
+        except Exception:
+            log.warning("Connection to Judo Water Treatment failed")
+            return None
+
+
+    
     async def connect(self):
         """Open REST connection to test if available."""
         res = await self.get_rest("FF00")
@@ -124,23 +144,49 @@ class RestObject:
         self._rest_item = rest_item
         self._rest_api = rest_api
 
-    def get_val(self, text: str, byte: int, byte_len: int) -> int:
-        index = byte * 2
-        big_endian = text[index : index + byte_len * 2]
-        little_endian = bytes.fromhex(big_endian)[::-1].hex()
-        return int(little_endian, 16)
+    def format_response(self, text: str, flip) -> str
+        index = self._rest_item.read_index * 2
+        big_endian = text[index : index + self._rest_item.read_bytes * 2]
+        if flip is True:
+            little_endian = bytes.fromhex(big_endian)[::-1].hex()
+            return little_endian
+        return big_endian
 
-    def get_status(self, text: str, byte: int, byte_len: int) -> str:
-        index = byte * 2
-        big_endian = text[index : index + byte_len * 2]
-        little_endian = bytes.fromhex(big_endian)[::-1].hex()
-        return self._rest_item.get_translation_key_from_number(int(little_endian, 16))
+    def format_int_message(self, number: int, flip) -> str
+        numbytes = str(self._rest_item.write_bytes * 2)
+        mask = "%0."+numbytes+"X"
+        big_endian = mask % integerVariable
+        if flip is True:
+            little_endian = bytes.fromhex(big_endian)[::-1].hex()
+            return little_endian
+        return big_endian
 
-    def get_text(self, text: str, byte: int, byte_len: int) -> str:
-        index = byte * 2
-        big_endian = text[index : index + byte_len * 2]
-        return bytearray.fromhex(big_endian).decode()
+    def format_str_message(self, text: str, flip) -> str
+        big_endian = text.encode("utf-8").hex()
+        if flip is True:
+            little_endian = bytes.fromhex(big_endian)[::-1].hex()
+            return little_endian
+        return big_endian
+    
+    
+    def get_val(self, text: str) -> float:
+        return float(int(self.format_response(text,True), 16)/self._rest_item._divider)
 
+    def get_status(self, text: str) -> str:
+        return self._rest_item.get_translation_key_from_number(int(self.format_response(text,True), 16))
+
+    def get_text(self, text: str) -> str:
+        return bytearray.fromhex(self.format_response(text,False)).decode()
+
+    def set_val(self, number: float) -> str:
+        return self.format_int_message(int(number*self._rest_item._divider),True)
+
+    def set_status(self, label: str) -> int:
+        return self.format_int_message(self._rest_item.get_number_from_translation_key(label),True)
+
+    def set_text(self, text: str) -> str:
+        return self.format_str_message(text,True)
+    
     @property
     async def value(self):
         """Returns the value from the REST API."""
@@ -150,17 +196,11 @@ class RestObject:
         res = await self._rest_api.get_rest(self._rest_item.address_read)
         match self._rest_item.format:
             case FORMATS.NUMBER:
-                return self.get_val(
-                    res, self._rest_item.read_index, self._rest_item.read_bytes
-                )
+                return self.get_val(res)
             case FORMATS.TEXT:
-                return self.get_text(
-                    res, self._rest_item.read_index, self._rest_item.read_bytes
-                )
+                return self.get_text(res)
             case FORMATS.STATUS:
-                return self.get_status(
-                    res, self._rest_item.read_index, self._rest_item.read_bytes
-                )
+                return self.get_status(res)
             case _:
                 log.warning(
                     "Unknown format: %s in %s",
@@ -175,7 +215,26 @@ class RestObject:
         """Set the value of the rest register, does nothing when not R/W.
 
         :param val: The value to write to the rest
-        :type val: int"""
+        :type val: any"""
+        towrite = None
         if self._rest_api is None:
             return
-        return None
+        if self._rest_item._type == TYPES.SENSOR:
+            return
+        match self._rest_item.format:
+            case FORMATS.NUMBER:
+                towrite = self.set_val(value)
+            case FORMATS.TEXT:
+                towrite = self.set_text(value)
+            case FORMATS.STATUS:
+                towrite = self.set_status(value)
+            case _:
+                log.warning(
+                    "Unknown format: %s in %s",
+                    str(self._rest_item.type),
+                    str(self._rest_item.translation_key),
+                )
+                return
+        if towrite is not None:
+            await self._rest_api.set_rest(self._rest_item.address_write, towrite)
+        return
